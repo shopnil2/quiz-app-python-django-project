d100aaa4aba65d50a7cef8326d75363d4395ea63
Arafat Rahman <tausifarafatshopnil70@gmail.com>
2025-02-24 20:33:18 +0600

    first commit

--- quiz/settings.py
+++ quiz/settings.py
@@ -22,13 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('KEY')
+SECRET_KEY = 'django-insecure-1234567890abcdefghijklmnopqrstuvwxyz1234567890'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['quiz-app-django.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
